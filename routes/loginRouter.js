10e57eea877c4cd93e3b22893b6fc7732d935c7e
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-07-25 04:35:49 +0000

    generate token in login post request

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import jwt from 'jsonwebtoken';
 
 const loginRouter = Router();
 
 loginRouter.post("/", async(req, res) => {
-
+    const user = {
+        username: req.body.username,
+    }
+    jwt.sign({user}, process.env.SECRET_KEY, (err, token) => {
+        res.json({token});
+    });
 });
 
 export default loginRouter;
--- routes/signupRouter.js
+++ routes/signupRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import bcrypt from "bcryptjs";
-import prisma from '../prisma/queries';
+import prisma from '../prisma/queries.js';
 
 const signupRouter = Router();
 
