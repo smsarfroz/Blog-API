7f6b9ad1cb14a7b787f1e49bf1563a4f11873a96
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-08-12 06:06:54 +0000

    test if the publish functionality work as a whole

--- controllers/updatePostbyid.js
+++ controllers/updatePostbyid.js
@@ -2,10 +2,10 @@ import prisma from '../prisma/queries.js';
 
 const updatePostbyid = async(req, res) => {
     try {
-        const { postid } = req.params;
-        const id = parseInt(postid);
+        const id = req.body.id;
         const published = req.body.published;
-    
+
+        // console.log(id, published);
         const updatePost = await prisma.updatePostbyid(id, published);
 
         res.status(200).json(updatePost);
